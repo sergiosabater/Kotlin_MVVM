5f01e0be4b085056224f7595b468967b6123f575
Sergio <sergiosabater@gmail.com>
2019-12-17 23:41:54 +0100

    Version 1.0
    Signed-off-by: Sergio <sergiosabater@gmail.com>

---
+++ app/src/main/java/com/developers/mvvmsample/utils/ISchedulerProvider.kt
@@ -0,0 +1,13 @@
+package com.developers.mvvmsample.utils
+
+import io.reactivex.Scheduler
+
+
+
+interface ISchedulerProvider {
+    fun ui(): Scheduler
+
+    fun computation(): Scheduler
+
+    fun io(): Scheduler
+}
